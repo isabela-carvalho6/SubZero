d8201ad75e4f2512023b2b582f7e1a259d618fe0
Victor-Queiroz-Mendes <victor.h.mendes7@aluno.senai.br>
2025-05-16 15:31:48 -0300

    atualização

--- VIEWS/DRINK_OLD_FASHIONED_VICTOR/oldfashioned.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const backButton = document.querySelector('.back-button');
-    
-    backButton.addEventListener('click', function(e) {
-        e.preventDefault();
-        alert('Voltando para a página anterior...');
-        // window.history.back(); // Descomente se quiser que funcione de verdade
-    });
-});
