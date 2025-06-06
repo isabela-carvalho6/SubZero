f128193e6e04a9163be4cf09571b6024687edb1a
Victor-Queiroz-Mendes <victor.h.mendes7@aluno.senai.br>
2025-06-06 07:51:09 -0300

    atualizando
    aaaaaa

--- views/feedback/feedback_form.php
+++ views/feedback/feedback_form.php
@@ -12,7 +12,7 @@
 
         <h1>Feedback</h1>
         
-        <form action="/dev_pub/save-feedback" method="POST">
+        <form action="/SubZero/public/save-feedback" method="POST">
         
             <label for="titulo">Título:</label>
             <input type="text" id="titulo" name="titulo" required><br><br>
@@ -30,9 +30,8 @@
         </form>
 
 
-        <a href="/dev_pub/list-feedback"><h4>Ver todos os feedbacks</h4></a>
-
-        <a href="http://localhost/dev_pub/public/feedback/"> Postar Feedback</a>
+        <a href="/SubZero/public/list-feedback"><h4>Ver todos os feedbacks</h4></a>
+        <a href="/SubZero/public/feedback/">Postar Feedback</a>
 
         <form method="POST" action="/dev_pub/delete-feedback">
             <input type="hidden" name="id_feedback" value="<?= $feedback['id_feedback'] ?>">
